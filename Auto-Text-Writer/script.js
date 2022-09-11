49228ac580d6cae203bb5834a7e173b2d367f3c7
twinkle-tyagi <twinkletyagi@niet.co.in>
2022-09-11 18:00:02 +0530

    Auto-text-writer

---
+++ Auto-Text-Writer/script.js
@@ -0,0 +1,13 @@
+const text = "this is sharpeners class"
+let index = 0;
+
+function writeText() {
+    document.body.innerText = text.slice(0, index);
+    index++;
+
+    if(index > text.length) {
+        index = 0;
+    }
+}
+
+setInterval(writeText, 100);
